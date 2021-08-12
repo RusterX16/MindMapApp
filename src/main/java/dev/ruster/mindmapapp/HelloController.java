cd92b922eae4bf934f65b4bd7b9d0a71ebd31489
BuildTools <unconfigured@null.spigotmc.org>
2021-08-12 19:22:41 +0200

    Initial commit

---
+++ src/main/java/dev/ruster/mindmapapp/HelloController.java
@@ -0,0 +1,15 @@
+package dev.ruster.mindmapapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
